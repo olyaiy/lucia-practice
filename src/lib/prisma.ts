5d9b2b64bf44c13d89571ca6b75e133dc8de19d1
olyaiy <alexolyaiy@gmail.com>
2024-09-18 14:17:13 -0700

    set global variable for prisma connection

---
+++ src/lib/prisma.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client'
+
+const client = new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error']
+})
+
+const globalForPrisma = globalThis as unknown as {
+    primsa: PrismaClient | undefined
+}
+
+export const prisma = globalForPrisma.primsa ?? client 
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.primsa = client
